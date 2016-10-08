ddff38ab7fca65d2f2caf9ab22d7ac8caa71d3e5
Girish S <girish.sonawane@gmail.com>
2016-10-08 20:13:30 +0530

    run elm repl in container

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.post('/terminals', (req, res) => {
   let cols = parseInt(req.query.cols),
       rows = parseInt(req.query.rows),
       repl_name = shortid.generate(),
-      term = pty.spawn('docker', ["run", "-it", "--rm", "--name", repl_name, "busybox"], {
+      term = pty.spawn('docker', ["run", "-it", "--name", repl_name, "--entrypoint", "elm-repl", "codesimple/elm:0.17"], {
         name: 'xterm-color',
         cols: cols || 80,
         rows: rows || 24,
