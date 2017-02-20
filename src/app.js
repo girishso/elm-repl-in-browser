74751b427ec4ddfa553f5d853e880b1daa915a2b
Girish S <girish.sonawane@gmail.com>
2017-02-20 09:26:03 +0530

    reformatted

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
 'use strict';
 
 const
-  express = require('express'),
-  app = express(),
-  expressWs = require('express-ws')(app),
-  os = require('os'),
-  pty = require('pty.js'),
-  shortid = require('shortid'),
-  spawn = require('child_process').spawn,
-  RateLimit = require('express-rate-limit');
+    express = require('express'),
+    app = express(),
+    expressWs = require('express-ws')(app),
+    os = require('os'),
+    pty = require('pty.js'),
+    shortid = require('shortid'),
+    spawn = require('child_process').spawn,
+    RateLimit = require('express-rate-limit');
 
 // rate limiter
 app.enable('trust proxy');
 
 
 let limiter = new RateLimit({
-  windowMs: 30*60*1000, // 30 minutes
-  delayAfter: 10, // begin slowing down responses after the first request
-  delayMs: 2*1000, // slow down subsequent responses by 2 seconds per request
-  max: 15, // start blocking after 30 requests
-  message: "Too many requests from this IP, please try again after 30 minutes"
+    windowMs: 30 * 60 * 1000, // 30 minutes
+    delayAfter: 10, // begin slowing down responses after the first request
+    delayMs: 2 * 1000, // slow down subsequent responses by 2 seconds per request
+    max: 15, // start blocking after 30 requests
+    message: "Too many requests from this IP, please try again after 30 minutes"
 });
 
 //  apply to all requests
@@ -31,7 +31,7 @@ let terminals = {},
 app.use(express.static(__dirname + '/public'));
 app.use("/xterm", express.static(__dirname + '/../node_modules/xterm'));
 
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
@@ -40,107 +40,96 @@ app.use(function(err, req, res, next) {
 });
 
 app.post('/terminals', (req, res) => {
-  let cols = parseInt(req.query.cols),
-      rows = parseInt(req.query.rows),
-      repl_name = shortid.generate(),
-      term = pty.spawn('docker', ["run", "-it", "-v", __dirname + "/../tmp:/code", "-w", "/code", "--name", repl_name, "--entrypoint", "elm-repl", "codesimple/elm:0.17"], {
-        name: 'xterm-color',
-        cols: cols || 80,
-        rows: rows || 32,
-        cwd: process.env.PWD,
-        env: process.env
-      });
+    let cols = parseInt(req.query.cols),
+        rows = parseInt(req.query.rows),
+        repl_name = shortid.generate(),
+        term = pty.spawn('docker', ["run", "-it", "-v", __dirname + "/../tmp:/code", "-w", "/code", "--name", repl_name, "--entrypoint", "elm-repl", "codesimple/elm:0.17"], {
+            name: 'xterm-color',
+            cols: cols || 80,
+            rows: rows || 32,
+            cwd: process.env.PWD,
+            env: process.env
+        });
 
-  console.log(`Created terminal with PID: ${term.pid}, container: ${repl_name}, size: ${cols}x${rows}`);
-  // console.log(simpleStringify(term));
-  terminals[repl_name] = term;
-  logs[repl_name] = '';
-  term.on('data', (data) => {
-    logs[repl_name] += data;
-  });
-  res.send(repl_name);
-  res.end();
+    console.log(`Created terminal with PID: ${term.pid}, container: ${repl_name}, size: ${cols}x${rows}`);
+    // console.log(simpleStringify(term));
+    terminals[repl_name] = term;
+    logs[repl_name] = '';
+    term.on('data', (data) => {
+        logs[repl_name] += data;
+    });
+    res.send(repl_name);
+    res.end();
 });
 
 app.post('/terminals/:repl_name/size', (req, res) => {
-  let term = terminals[req.params.repl_name],
-      pid = parseInt(term.pid),
-      cols = parseInt(req.query.cols),
-      rows = parseInt(req.query.rows);
+    let term = terminals[req.params.repl_name],
+        pid = parseInt(term.pid),
+        cols = parseInt(req.query.cols),
+        rows = parseInt(req.query.rows);
 
-  term.resize(cols, rows);
-  console.log('Resized terminal ' + req.params.repl_name + ' to ' + cols + ' cols and ' + rows + ' rows.');
-  res.end();
+    term.resize(cols, rows);
+    console.log('Resized terminal ' + req.params.repl_name + ' to ' + cols + ' cols and ' + rows + ' rows.');
+    res.end();
 });
 
 app.ws('/ws/:repl_name', (ws, req) => {
-  let repl_name = req.params.repl_name, term = terminals[repl_name];
-  console.log('Connected to terminal ' + repl_name);
+    let repl_name = req.params.repl_name,
+        term = terminals[repl_name];
+    console.log('Connected to terminal ' + repl_name);
 
-  try {
-    ws.send(logs[repl_name]);
-  } catch (ex) {
-    console.error(ex)
-    ws.close();
-  }
-
-  term.on('data', (data) => {
     try {
-      ws.send(data);
+        ws.send(logs[repl_name]);
     } catch (ex) {
-      // The WebSocket is not open, ignore
+        console.error(ex)
+        ws.close();
     }
-  });
 
-  term.on('exit', (code, signal) => {
-    try {
-      ws.close();
-    } catch (ex) {
-      console.error(ex)
-    }
-  });
+    term.on('data', (data) => {
+        try {
+            ws.send(data);
+        } catch (ex) {
+            // The WebSocket is not open, ignore
+        }
+    });
 
-  // not executed
-  term.on('error', function() {
-    console.log("term error");
-  });
+    term.on('exit', (code, signal) => {
+        try {
+            ws.close();
+        } catch (ex) {
+            console.error(ex)
+        }
+    });
 
-  ws.on('message', (msg) => {
-    try {
-      term.write(msg);
-    } catch (ex) {
-      console.error(ex)
-      ws.close();
-    }
-  });
+    ws.on('message', (msg) => {
+        try {
+            term.write(msg);
+        } catch (ex) {
+            console.error(ex)
+            ws.close();
+        }
+    });
 
-  // not executed
-  ws.on('error', function() {
-    console.log("Communication error")
-    term.write("\rFail: ");
-    term.write("Communication error");
-  });
+    ws.on('close', () => {
+        try {
+            let stop_container = spawn('docker', ["rm", "-f", repl_name]);
+            stop_container.on('close', code => {
+                console.log("stop_container exited with code " + code);
+            });
+            stop_container.stdout.on('data', data => {
+                console.log(`stdout: ${data}`);
+            });
 
-  ws.on('close',  () => {
-    try {
-      let stop_container = spawn('docker', ["rm", "-f", repl_name]);
-      stop_container.on( 'close', code => {
-          console.log("stop_container exited with code " + code );
-      });
-      stop_container.stdout.on( 'data', data => {
-          console.log( `stdout: ${data}` );
-      });
-
-      stop_container.stderr.on( 'data', data => {
-          console.log( `stderr: ${data}` );
-      });
-      // Clean things up
-      delete terminals[repl_name];
-      delete logs[repl_name];
-    } catch(ex) {
-      console.error(ex)
-    }
-  });
+            stop_container.stderr.on('data', data => {
+                console.log(`stderr: ${data}`);
+            });
+            // Clean things up
+            delete terminals[repl_name];
+            delete logs[repl_name];
+        } catch (ex) {
+            console.error(ex)
+        }
+    });
 });
 
 var port = process.env.PORT || 3000,
@@ -153,22 +142,17 @@ app.listen(port, host);
 
 let simpleStringify = (object) => {
     let simpleObject = {};
-    for (let prop in object ){
-        if (!object.hasOwnProperty(prop)){
+    for (let prop in object) {
+        if (!object.hasOwnProperty(prop)) {
             continue;
         }
-        if (typeof(object[prop]) == 'object'){
+        if (typeof(object[prop]) == 'object') {
             continue;
         }
-        if (typeof(object[prop]) == 'function'){
+        if (typeof(object[prop]) == 'function') {
             continue;
         }
         simpleObject[prop] = object[prop];
     }
     return JSON.stringify(simpleObject); // returns cleaned up JSON
 };
-
-// very crude implementation
-process.on('uncaughtException', (err) => {
-  console.error(1, `Caught exception: ${err}`);
-});
